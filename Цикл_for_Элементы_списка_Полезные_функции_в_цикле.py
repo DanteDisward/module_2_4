71c7f0915ad855fbfa1e87fafc31e6564bc7a749
DanteDisward <nikitachernigin@gmail.com>
2024-08-07 10:18:45 +0500

    V0.1

--- Цикл_for_Элементы_списка_Полезные_функции_в_цикле.py
+++ Цикл_for_Элементы_списка_Полезные_функции_в_цикле.py
@@ -1,3 +1,16 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
+for item in numbers:
+    remains = 0
+    for i in range(item, 0, -1):
+        if item % i == 0:
+            remains += 1
+        if remains > 2:
+            break
+    if remains > 2:
+        not_primes.append(item)
+    elif remains == 2:
+        primes.append(item)
+print(primes)
+print(not_primes)
